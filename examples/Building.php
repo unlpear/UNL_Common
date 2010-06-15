9a6de9581f77afa58f66c8634356c1f9a0f4a86d
Brett Bieber <brett.bieber@gmail.com>
2010-06-15 15:09:10 +0000

    Use local files before others in the include path.

--- examples/Building.php
+++ examples/Building.php
@@ -6,6 +6,11 @@
  * @package UNL_Common
  * Created on Sep 27, 2005
  */
+
+chdir(dirname(__FILE__).'/../');
+
+set_include_path(dirname(dirname(__FILE__)).'/src'.PATH_SEPARATOR.get_include_path());
+
 require_once 'UNL/Common/Building.php';
 require_once 'HTML/QuickForm.php';
 $bldgs = new UNL_Common_Building();
