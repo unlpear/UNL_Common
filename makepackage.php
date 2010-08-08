e486e05f672c8de18a1b4915bd03ae3bd6e8df88
Brett Bieber <brett.bieber@gmail.com>
2010-08-08 12:03:02 +0000

    Prep for 0.6.0 release

--- makepackage.php
+++
@@ -1,9 +0,0 @@
-<?php
-ini_set('display_errors',true);
-
-unlink('data/unl_common.sqlite');
-set_include_path(dirname(__FILE__).'/src');
-require_once 'UNL/Common/Building/City.php';
-require_once 'UNL/Common/Building/East.php';
-$c = new UNL_Common_Building_City();
-$e = new UNL_Common_Building_East();
